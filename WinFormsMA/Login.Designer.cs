18bca6586ba39f0f42bc1d58f3788ed80c6d228e
jordibonet <jboneta2122@politecnics.barcelona>
2024-10-23 19:48:02 +0200

    add base of log in

--- WinFormsMA/Login.Designer.cs
+++ WinFormsMA/Login.Designer.cs
@@ -29,17 +29,75 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
-            label1 = new Label();
+            labelTitle = new Label();
+            labelText = new Label();
+            labelPassword = new Label();
+            textBoxText = new TextBox();
+            textBoxPassword = new TextBox();
+            buttonLogIn = new Button();
             SuspendLayout();
             // 
-            // label1
+            // labelTitle
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(334, 185);
-            label1.Name = "label1";
-            label1.Size = new Size(38, 15);
-            label1.TabIndex = 0;
-            label1.Text = "label1";
+            labelTitle.AutoSize = true;
+            labelTitle.BackColor = Color.Transparent;
+            labelTitle.Font = new Font("Comic Sans MS", 35F, FontStyle.Bold);
+            labelTitle.ForeColor = Color.Black;
+            labelTitle.Location = new Point(146, 67);
+            labelTitle.Name = "labelTitle";
+            labelTitle.Size = new Size(537, 65);
+            labelTitle.TabIndex = 0;
+            labelTitle.Text = "MANETES ARTISTES";
+            // 
+            // labelText
+            // 
+            labelText.AutoSize = true;
+            labelText.BackColor = Color.Transparent;
+            labelText.Font = new Font("Segoe UI", 18F);
+            labelText.Location = new Point(168, 207);
+            labelText.Name = "labelText";
+            labelText.Size = new Size(57, 32);
+            labelText.TabIndex = 1;
+            labelText.Text = "Text";
+            // 
+            // labelPassword
+            // 
+            labelPassword.AutoSize = true;
+            labelPassword.BackColor = Color.Transparent;
+            labelPassword.Font = new Font("Segoe UI", 18F);
+            labelPassword.Location = new Point(146, 257);
+            labelPassword.Name = "labelPassword";
+            labelPassword.Size = new Size(111, 32);
+            labelPassword.TabIndex = 2;
+            labelPassword.Text = "Password";
+            // 
+            // textBoxText
+            // 
+            textBoxText.Font = new Font("Segoe UI", 12F);
+            textBoxText.Location = new Point(281, 213);
+            textBoxText.Name = "textBoxText";
+            textBoxText.Size = new Size(299, 29);
+            textBoxText.TabIndex = 3;
+            // 
+            // textBoxPassword
+            // 
+            textBoxPassword.Font = new Font("Segoe UI", 12F);
+            textBoxPassword.Location = new Point(281, 266);
+            textBoxPassword.Name = "textBoxPassword";
+            textBoxPassword.Size = new Size(299, 29);
+            textBoxPassword.TabIndex = 4;
+            textBoxPassword.UseSystemPasswordChar = true;
+            // 
+            // buttonLogIn
+            // 
+            buttonLogIn.FlatAppearance.BorderColor = Color.Black;
+            buttonLogIn.Font = new Font("Segoe UI", 20F);
+            buttonLogIn.Location = new Point(331, 350);
+            buttonLogIn.Name = "buttonLogIn";
+            buttonLogIn.Size = new Size(140, 62);
+            buttonLogIn.TabIndex = 5;
+            buttonLogIn.Text = "Log In";
+            buttonLogIn.UseVisualStyleBackColor = true;
             // 
             // Login
             // 
@@ -48,7 +106,12 @@
             BackgroundImage = Properties.Resources.login_background_v1;
             BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(800, 450);
-            Controls.Add(label1);
+            Controls.Add(buttonLogIn);
+            Controls.Add(textBoxPassword);
+            Controls.Add(textBoxText);
+            Controls.Add(labelPassword);
+            Controls.Add(labelText);
+            Controls.Add(labelTitle);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Login";
             Text = "ManetesArtistes";
@@ -59,6 +122,11 @@
 
         #endregion
 
-        private Label label1;
+        private Label labelTitle;
+        private Label labelText;
+        private Label labelPassword;
+        private TextBox textBoxText;
+        private TextBox textBoxPassword;
+        private Button buttonLogIn;
     }
 }
--- WinFormsMA/obj/Debug/net8.0-windows/WinFormsMA.AssemblyInfo.cs
+++ WinFormsMA/obj/Debug/net8.0-windows/WinFormsMA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsMA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1296edf73a98f52a8452a430daaeca92aba5b89d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+884136954ec8921211d751566e18fe811f38e0fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsMA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsMA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
