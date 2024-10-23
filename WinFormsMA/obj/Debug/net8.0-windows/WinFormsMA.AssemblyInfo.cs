d7f619ec0faeda80f85932ad1eff9993e7d9d5ac
rwxce <148581386+rwxce@users.noreply.github.com>
2024-10-23 15:59:37 +0200

    add base

--- WinFormsMA/obj/Debug/net8.0-windows/WinFormsMA.AssemblyInfo.cs
+++ WinFormsMA/obj/Debug/net8.0-windows/WinFormsMA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsMA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66d6ed565c4505fbe8b31ce4ef12d080df6d00d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsMA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsMA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
